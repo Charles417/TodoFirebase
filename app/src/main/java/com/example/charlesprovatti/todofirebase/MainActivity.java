62f40a36e2e5c28d0c90bcae0fe8f4e805ccfbbd
Charles417 <cprovatti@yahoo.com.br>
2019-05-09 11:53:06 -0300

    Crud completo.

--- app/src/main/java/com/example/charlesprovatti/todofirebase/MainActivity.java
+++ app/src/main/java/com/example/charlesprovatti/todofirebase/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.charlesprovatti.todofirebase;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.charlesprovatti.todofirebase.modelo.Tarefa;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    private ListView listView;
+    private List<Tarefa> listaTarefas = new ArrayList<Tarefa>();
+    private ArrayAdapter<Tarefa> arrayAdapterTarefa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        conectarBanco();
+        eventoBanco();
     }
 
     @Override
@@ -37,5 +64,59 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void conectarBanco()
+    {
+        FirebaseApp.initializeApp(MainActivity.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    private void eventoBanco()
+    {
+        databaseReference.child("Tarefa").addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                listaTarefas.clear();
+                for (DataSnapshot snapshot:dataSnapshot.getChildren())
+                {
+                    Tarefa tarefa = snapshot.getValue(Tarefa.class);
+                    listaTarefas.add(tarefa);
+                }
+
+                arrayAdapterTarefa = new ArrayAdapter<Tarefa>(MainActivity.this, R.layout.layout_lista, R.id.tvNomeTarefa, listaTarefas);
+                listView.setAdapter(arrayAdapterTarefa);
+
+                listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        Tarefa tarefaSelecionada = (Tarefa)parent.getItemAtPosition(position);
+                        databaseReference.child("Tarefa").child(tarefaSelecionada.getUid()).removeValue();
+                        return false;
+                    }
+                });
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Tarefa tarefaSelecionada = (Tarefa)parent.getItemAtPosition(position);
+                        Intent intent = new Intent(MainActivity.this, AdicionarActivity.class);
+                        intent.putExtra("uid",tarefaSelecionada.getUid());
+                        intent.putExtra("nome", tarefaSelecionada.getNome());
+                        startActivity(intent);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
